f381c612dc1b297c85d210b291906ab092dae506
王宪鹏 <wangxianpeng@lixiang.com>
2023-02-07 10:29:03 +0800

    fix: update api key

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -1,10 +1,10 @@
 
-import superagent from "superagent"
+const superagent = require("superagent")
 
 const completionsUrl = 'https://api.openai.com/v1/completions'
 const headers = {
   "Content-Type": "application/json",
-  "Authorization": "Bearer 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",
+  "Authorization": "Bearer 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",
 }
 
 const getConfig = (words) => ({
@@ -22,8 +22,12 @@ export default async function handler(req, res) {
   superagent.post(completionsUrl)
     .send(body)
     .set('Content-Type', "application/json")
-    .set('Authorization', "Bearer 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")
+    .set('Authorization', "Bearer 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")
     .end((err, response) => {
+      console.log({
+        err,
+        response
+      })
       const data = err ? '服务正忙，请稍后再试...' : JSON.parse(response.text).choices[0].text
       res.status(200).json({ data })
     })
